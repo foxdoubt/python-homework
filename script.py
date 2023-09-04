33d9aa37e723af5fda87d2f2a6b0ce592774b3b8
“Daniel <danielwdewald@gmail.com>
2023-09-03 21:43:01 -0400

    Add professions variable

--- script.py
+++ script.py
@@ -4,6 +4,8 @@ data = pd.read_csv('github-course-data.csv')
 
 firstnames = data['firstname'].values
 lastnames = data['lastname'].values
+professions = data['professions'].values
 
 print(firstnames)
 print(lastnames)
+print(professions)
