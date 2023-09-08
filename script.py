c664793aa5c3ffd212470fa80d1157cda022ef3b
“Daniel <danielwdewald@gmail.com>
2023-09-08 11:48:07 -0400

    Latest change

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ df = pd.read_csv('github-course-data.csv')
 
 condition_1 = df.profession == 'worker'
 condition_2 = df.profession == 'firefighter'
-first_and_last_names = ['firstname','lastname']
+first_and_last_names = ['firstname','last_name']
 query_result = df[condition_1 | condition_2]
 
 print(query_result.to_string())
