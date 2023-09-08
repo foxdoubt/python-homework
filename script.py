5b79633f9e276c778135a626f1445b513782987c
“Daniel <danielwdewald@gmail.com>
2023-09-08 18:59:13 -0400

    Query for developer or firefighter

--- script.py
+++ script.py
@@ -4,4 +4,8 @@ df = pd.read_csv('github-course-data.csv')
 
 first_and_last_names = df[['firstname', 'lastname']]
 
+condition_1 = df['profession'] == 'developer'
+condition_2 = df['profession'] == 'firefighter'
+
+print(df[condition_1 | condition_2])
 print(first_and_last_names)
