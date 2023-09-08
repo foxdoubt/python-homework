d5d73bd203886b6d44a7ac8511adfd4c5094e266
“Daniel <danielwdewald@gmail.com>
2023-09-08 11:33:16 -0400

    Add query for profession type

--- script.py
+++ script.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv('github-course-data.csv')
-info = df.info()
-print(info)
+
+condition_1 = df.profession == 'worker'
+condition_2 = df.profession == 'firefighter'
+
+query_result = df[condition_1 | condition_2]
+print(query_result.to_string())
